2b6a6b46358732f839c60ab13d437e29d2d2fe4d
LesterOper <51096246+LesterOper@users.noreply.github.com>
2019-11-13 01:00:40 +0300

    Here test for HttpSessionCartService Forgot to add this test in last commit

---
+++ src/test/java/com/es/phoneshop/Session/HttpSessionCartServiceTest.java
@@ -0,0 +1,37 @@
+package com.es.phoneshop.Session;
+
+import com.es.phoneshop.Cart.Cart;
+import com.es.phoneshop.Cart.CartService;
+import com.es.phoneshop.Cart.HttpSessionCartService;
+import com.es.phoneshop.model.product.ArrayListProductDao;
+import com.es.phoneshop.model.product.Product;
+import com.es.phoneshop.model.product.ProductDao;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.*;
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class HttpSessionCartServiceTest {
+    private CartService cartService;
+    private ProductDao product;
+
+    @Before
+    public void setup() {
+        cartService = HttpSessionCartService.getInstance();
+        product = ArrayListProductDao.getInstance();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddProductToCart() {
+        Currency usd = Currency.getInstance("USD");
+        Cart cart = new Cart();
+        Product item = new Product(4L, "iphone", "Apple iPhone", new BigDecimal(200), usd, 10, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Apple/Apple%20iPhone.jpg");
+        int quantity = 3;
+        cartService.addProduct(cart, item, quantity);
+        cartService.addProduct(cart, item, quantity + 6);
+        assertNotNull(cart.getList());
+    }
+}
